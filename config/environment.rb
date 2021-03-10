922e9f2fa7a6747ab7cd34a24c31a5ddd2b944cb
Usman Kabir <usman.kabir.k@gmail.com>
2021-03-10 21:38:49 +0500

    set smtp host

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,13 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+if Rails.env.development?
+  ActionMailer::Base.default_url_options = { host: ENV["HOST"], port: 3000 }
+elsif Rails.env.production?
+  ActionMailer::Base.default_url_options = { host: ENV["HOST"] }
+end
+
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
