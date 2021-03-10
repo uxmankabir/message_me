f0440a4851d2bd545f5eafc0ca753f390ce695d5
Usman Kabir <usman.kabir.k@gmail.com>
2021-03-10 21:11:52 +0500

    smtp settings for producation

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               ENV["DOMAIN"],
+  user_name:            ENV["gmail_username"],
+  password:             ENV["gmail_password"],
+  authentication:       'plain',
+  enable_starttls_auto: true
+}
