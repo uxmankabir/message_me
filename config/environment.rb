53bbdf976ec909f6ca29cf0301c0b552e2a9b0d6
Usman Kabir <usman.kabir.k@gmail.com>
2021-03-10 23:06:47 +0500

    configuring action_mailer

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,6 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-if Rails.env.development?
-  ActionMailer::Base.default_url_options = { host: ENV["HOST_NAME"], port: 3000 }
-elsif Rails.env.production?
-  ActionMailer::Base.default_url_options = { host: ENV["HOST_NAME"] }
-end
-
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   address:              'smtp.gmail.com',
